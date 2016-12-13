453fb01734e49b2c3bc2c5aeefefbb4d90f0460c
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2016-12-13 21:50:13 +0000

    Prototyped the colour compilation unit

---
+++ src/colour.cpp
@@ -0,0 +1,24 @@
+#include "colour.hpp"
+
+
+namespace hexago {
+
+    // returns an RGBA sf::Color instance for the given HSV+A colour
+    sf::Color colour_from_hsv_a(hsv_colour_t hsv_a_colour) {
+        // TODO: Write Code!
+    }
+
+    // returns an RGBA sf::Color instance for the given LAB+A colour
+    sf::Color colour_from_lab_a(lab_colour_t lab_a_colour) {
+        // TODO: Write Code!
+    }
+
+    /*
+     * private function, returns a CIE 1931 (XYZ) + Alpha colour instance for
+     * the given LAB+A colour
+     */
+    static xyz_colour_t xyz_a_from_lab_a(xyz_colour_t xyz_a_colour) {
+        // TODO: Write Code!
+    }
+
+}
---
+++ src/colour.hpp
@@ -0,0 +1,55 @@
+#ifndef SAXBOPHONE_HEXAGO_COLOUR_HPP
+#define SAXBOPHONE_HEXAGO_COLOUR_HPP
+
+#include <cstdint>
+
+#include <SFML/Graphics/Color.hpp>
+
+
+namespace hexago {
+
+    /*
+     * This struct type is used to represent a HSV+Alpha Colour
+     */
+    typedef struct hsv_colour_t {
+        // all values are in range 0.0 -> 1.0
+        float h; // hue
+        float s; // saturation
+        float v; // value
+        float alpha; // alpha
+    } hsv_colour_t;
+
+    /*
+     * This struct type is used to represent a XYZ+Alpha Colour
+     * (according to the CIE 1931 Colour Space).
+     *
+     * This type is intended to be used as an intermediate colour space type
+     * for converting between the LAB and RGB Colour spaces, as there is no
+     * defined calculation for converting directly between LAB and RGB
+     */
+    typedef struct xyz_colour_t {
+        float x; // range is 0.0 -> 95.047
+        float y; // range is 0.0 -> 100.000
+        float z; // range is 0.0 -> 108.883
+        float alpha; // range is 0.0 -> 1.0
+    } xyz_colour_t;
+
+    /*
+     * This struct type is used to represent a CIE-L*ab+Alpha Colour
+     */
+    typedef struct lab_colour_t {
+        uint8_t l; // lightness, with range 0 -> 100
+        int8_t a; // a component, with range -100 -> 100
+        int8_t b; // b component, with range -100 -> 100
+        float alpha; // range is 0.0 -> 1.0
+    } lab_colour_t;
+
+    // returns an RGBA sf::Color instance for the given HSV+A colour
+    sf::Color colour_from_hsv_a(hsv_colour_t hsv_a_colour);
+
+    // returns an RGBA sf::Color instance for the given LAB+A colour
+    sf::Color colour_from_lab_a(lab_colour_t lab_a_colour);
+
+}
+
+#endif // include guard
