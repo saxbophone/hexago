efbcc14e7660646a8d2ba11dbf2eb1cac8ee0ea2
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2017-03-04 18:54:06 +0000

    Refactor out loading of individual arguments for more DRY code

--- src/ArgumentParser.cpp
+++ src/ArgumentParser.cpp
@@ -11,6 +11,109 @@
 namespace hexago {
 
     /*
+     * module-private function for loading one command-line argument into a
+     * variable of type double
+     * returns true if the argument was found
+     */
+    static bool load_double_arg(
+        std::string flag,
+        std::string argument,
+        std::string next_argument,
+        double& destination,
+        int& advance
+    ) {
+        // if the argument is the same as the flag
+        if(argument == flag) {
+            // convert the next argument to double and store at destination
+            destination = std::stod(next_argument, NULL);
+            // set advance to 1 so we skip the next argument (already handled)
+            advance = 1;
+            // return true as we found it
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     * module-private function for loading one command-line argument which
+     * specifies the colour model to use
+     * returns true if the argument was found
+     */
+    static bool load_colour_arg(
+        std::string flag,
+        std::string argument,
+        hexago::colour_model_t colour_model,
+        hexago::colour_model_t& destination
+    ) {
+        // if the argument is the same as the flag
+        if(argument == flag) {
+            // store the specified colour model in the destination
+            destination = colour_model;
+            // return true as we found it
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     * module-private function for loading one command-line argument into a
+     * variable of type unsigned int
+     * returns true if the argument was found
+     */
+    static bool load_uint_arg(
+        std::string flag,
+        std::string argument,
+        std::string next_argument,
+        unsigned int& destination,
+        int& advance
+    ) {
+        // if the argument is the same as the flag
+        if(argument == flag) {
+            // convert the next argument to double and store at destination
+            destination = std::stoul(next_argument, NULL);
+            // set advance to 1 so we skip the next argument (already handled)
+            advance = 1;
+            // return true as we found it
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /*
+     * module-private function for loading one command-line argument which
+     * specifies the spawn mode to use
+     * returns true if the argument was found
+     */
+    static bool load_spawn_arg(
+        std::string flag,
+        std::string argument,
+        std::string next_argument,
+        hexago::hexagon_spawn_mode_t& destination,
+        int& advance
+    ) {
+        if(argument == flag) {
+            // set spawn mode based on string
+            if(next_argument == "default") {
+                destination = SPAWN_MODE_DEFAULT;
+            } else if(next_argument == "bottom") {
+                destination = SPAWN_MODE_BOTTOM;
+            } else if(next_argument == "top") {
+                destination = SPAWN_MODE_TOP;
+            }
+            // advance by one argument
+            advance = 1;
+            // return true as we found it
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
      * module-private function for parsing an argument and modifying config in
      * place - returns the number of indexes to skip in the arguments array
      */
@@ -31,113 +134,56 @@ namespace hexago {
          */
         if((argument[0] == '-') && ((next) < arg_count)) {
             // check its value for anything meaningful
-            if(argument == "-ssmin") {
-                // convert to double
-                config.start_size_range.min = std::stod(next_argument, NULL);
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-ssmax") {
-                // convert to double
-                config.start_size_range.max = std::stod(next_argument, NULL);
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-dsmin") {
-                // convert to double
-                config.decay_speed_range.min = std::stod(next_argument, NULL);
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-dsmax") {
-                // convert to double
-                config.decay_speed_range.max = std::stod(next_argument, NULL);
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-cmrgb") {
-                // set colour model to RGB
-                config.colour_model = COLOUR_MODEL_RGB;
-            } else if(argument == "-cmhsv") {
-                // set colour model to HSV
-                config.colour_model = COLOUR_MODEL_HSV;
-            } else if(argument == "-cmlab") {
-                // set colour model to LAB
-                config.colour_model = COLOUR_MODEL_LAB;
-            } else if(argument == "-dcmin") {
-                // convert to double
-                config.d_colour_channel_range.min = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-dcmax") {
-                // convert to double
-                config.d_colour_channel_range.max = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-ecmin") {
-                // convert to double
-                config.e_colour_channel_range.min = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-ecmax") {
-                // convert to double
-                config.e_colour_channel_range.max = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-fcmin") {
-                // convert to double
-                config.f_colour_channel_range.min = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-fcmax") {
-                // convert to double
-                config.f_colour_channel_range.max = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-acmin") {
-                // convert to double
-                config.alpha_colour_channel_range.min = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-acmax") {
-                // convert to double
-                config.alpha_colour_channel_range.max = std::stod(
-                    next_argument, NULL
-                );
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-frame") {
-                // convert to unsigned int
-                config.framerate = std::stoul(next_argument, NULL);
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-mincv") {
-                // convert to double
-                config.minimum_screen_cover = std::stod(next_argument, NULL);
-                // advance by one argument
-                advance = 1;
-            } else if(argument == "-spawn") {
-                // set spawn mode based on string
-                if(next_argument == "default") {
-                    config.spawn_mode = SPAWN_MODE_DEFAULT;
-                } else if(next_argument == "bottom") {
-                    config.spawn_mode = SPAWN_MODE_BOTTOM;
-                } else if(next_argument == "top") {
-                    config.spawn_mode = SPAWN_MODE_TOP;
-                }
-                // advance by one argument
-                advance = 1;
-            }
+            load_double_arg(
+                "-ssmin", argument, next_argument, config.start_size_range.min,
+                advance
+            ) || load_double_arg(
+                "-ssmax", argument, next_argument, config.start_size_range.max,
+                advance
+            ) || load_double_arg(
+                "-dsmin", argument, next_argument, config.decay_speed_range.min,
+                advance
+            ) || load_double_arg(
+                "-dsmax", argument, next_argument, config.decay_speed_range.max,
+                advance
+            ) || load_colour_arg(
+                "-cmrgb", argument, COLOUR_MODEL_RGB, config.colour_model
+            ) || load_colour_arg(
+                "-cmhsv", argument, COLOUR_MODEL_HSV, config.colour_model
+            ) || load_colour_arg(
+                "-cmlab", argument, COLOUR_MODEL_LAB, config.colour_model
+            ) || load_double_arg(
+                "-dcmin", argument, next_argument,
+                config.d_colour_channel_range.min, advance
+            ) || load_double_arg(
+                "-dcmax", argument, next_argument,
+                config.d_colour_channel_range.max, advance
+            ) || load_double_arg(
+                "-ecmin", argument, next_argument,
+                config.e_colour_channel_range.min, advance
+            ) || load_double_arg(
+                "-ecmax", argument, next_argument,
+                config.e_colour_channel_range.max, advance
+            ) || load_double_arg(
+                "-fcmin", argument, next_argument,
+                config.f_colour_channel_range.min, advance
+            ) || load_double_arg(
+                "-fcmax", argument, next_argument,
+                config.f_colour_channel_range.max, advance
+            ) || load_double_arg(
+                "-acmin", argument, next_argument,
+                config.alpha_colour_channel_range.min, advance
+            ) || load_double_arg(
+                "-acmax", argument, next_argument,
+                config.alpha_colour_channel_range.max, advance
+            ) || load_uint_arg(
+                "-frame", argument, next_argument, config.framerate, advance
+            ) || load_double_arg(
+                "-mincv", argument, next_argument, config.minimum_screen_cover,
+                advance
+            ) || load_spawn_arg(
+                "-spawn", argument, next_argument, config.spawn_mode, advance
+            );
         }
         return advance;
     }
