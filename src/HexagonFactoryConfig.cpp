c73fafea38bb99ba94253c70d6dcd44ba77c2f08
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2017-03-11 01:19:31 +0000

    Refactor out a large chunk of resolve_defaults to helper function

--- src/HexagonFactoryConfig.cpp
+++ src/HexagonFactoryConfig.cpp
@@ -11,6 +11,50 @@
 
 namespace hexago {
 
+    // stores the range bounds of a given colour model in the referenced vars
+    static void get_colour_model_range(
+        colour_model_t colour_model,
+        ParameterRange<colour_channel_t>& d_channel_bounds,
+        ParameterRange<colour_channel_t>& e_channel_bounds,
+        ParameterRange<colour_channel_t>& f_channel_bounds
+    ) {
+        switch(colour_model) {
+            case COLOUR_MODEL_RGB:
+                // r
+                d_channel_bounds.min = COLRCV_RGB_MIN_VALUE;
+                d_channel_bounds.max = COLRCV_RGB_MAX_VALUE;
+                // g
+                e_channel_bounds.min = COLRCV_RGB_MIN_VALUE;
+                e_channel_bounds.max = COLRCV_RGB_MAX_VALUE;
+                // b
+                f_channel_bounds.min = COLRCV_RGB_MIN_VALUE;
+                f_channel_bounds.max = COLRCV_RGB_MAX_VALUE;
+                break;
+            case COLOUR_MODEL_HSV:
+                // h
+                d_channel_bounds.min = COLRCV_HSV_MIN_VALUE;
+                d_channel_bounds.max = COLRCV_HSV_H_MAX_VALUE;
+                // s
+                e_channel_bounds.min = COLRCV_HSV_MIN_VALUE;
+                e_channel_bounds.max = COLRCV_HSV_S_MAX_VALUE;
+                // v
+                f_channel_bounds.min = COLRCV_HSV_MIN_VALUE;
+                f_channel_bounds.max = COLRCV_HSV_V_MAX_VALUE;
+                break;
+            case COLOUR_MODEL_LAB:
+                // l
+                d_channel_bounds.min = COLRCV_LAB_L_MIN_VALUE;
+                d_channel_bounds.max = COLRCV_LAB_MAX_VALUE;
+                // a
+                e_channel_bounds.min = COLRCV_LAB_A_MIN_VALUE;
+                e_channel_bounds.max = COLRCV_LAB_MAX_VALUE;
+                // b
+                f_channel_bounds.min = COLRCV_LAB_B_MIN_VALUE;
+                f_channel_bounds.max = COLRCV_LAB_MAX_VALUE;
+                break;
+        }
+    }
+
     // sets a given channel value to the value of check if it is NAN
     static void clamp_if_nan(colour_channel_t& input, colour_channel_t check) {
         if(std::isnan(input)) {
@@ -48,41 +92,12 @@ namespace hexago {
         ParameterRange<colour_channel_t> e_channel_bounds;
         ParameterRange<colour_channel_t> f_channel_bounds;
         // check which colour model we're using, set bounds accordingly
-        switch(this->colour_model) {
-            case COLOUR_MODEL_RGB:
-                // r
-                d_channel_bounds.min = COLRCV_RGB_MIN_VALUE;
-                d_channel_bounds.max = COLRCV_RGB_MAX_VALUE;
-                // g
-                e_channel_bounds.min = COLRCV_RGB_MIN_VALUE;
-                e_channel_bounds.max = COLRCV_RGB_MAX_VALUE;
-                // b
-                f_channel_bounds.min = COLRCV_RGB_MIN_VALUE;
-                f_channel_bounds.max = COLRCV_RGB_MAX_VALUE;
-                break;
-            case COLOUR_MODEL_HSV:
-                // h
-                d_channel_bounds.min = COLRCV_HSV_MIN_VALUE;
-                d_channel_bounds.max = COLRCV_HSV_H_MAX_VALUE;
-                // s
-                e_channel_bounds.min = COLRCV_HSV_MIN_VALUE;
-                e_channel_bounds.max = COLRCV_HSV_S_MAX_VALUE;
-                // v
-                f_channel_bounds.min = COLRCV_HSV_MIN_VALUE;
-                f_channel_bounds.max = COLRCV_HSV_V_MAX_VALUE;
-                break;
-            case COLOUR_MODEL_LAB:
-                // l
-                d_channel_bounds.min = COLRCV_LAB_L_MIN_VALUE;
-                d_channel_bounds.max = COLRCV_LAB_MAX_VALUE;
-                // a
-                e_channel_bounds.min = COLRCV_LAB_A_MIN_VALUE;
-                e_channel_bounds.max = COLRCV_LAB_MAX_VALUE;
-                // b
-                f_channel_bounds.min = COLRCV_LAB_B_MIN_VALUE;
-                f_channel_bounds.max = COLRCV_LAB_MAX_VALUE;
-                break;
-        }
+        get_colour_model_range(
+            this->colour_model,
+            d_channel_bounds,
+            e_channel_bounds,
+            f_channel_bounds
+        );
         // check any channel values for NANs and set them to bounds if so
         clamp_if_nan(this->d_colour_channel_range.min, d_channel_bounds.min);
         clamp_if_nan(this->d_colour_channel_range.max, d_channel_bounds.max);
