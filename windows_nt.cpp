a14485cbdd6f3309a4fe963bc91229c5df64efee
Joshua Saxby <joshua.a.saxby+UMvLnvbsOxBHaeiCHvbdunpz@gmail.com>
2018-01-25 21:10:58 +0000

    Rewrote the windows screensaver to properly integrate with the Windows screensaver API This has massively simplified that program as a result, although I had to make some minor changes to the hexago library in order to achieve this: - Change the constructor of HexagoScreenSaver to take a pointer to an SFML::RenderWindow instance instead of a reference to one --I'm considering refactoring that class to take a window handle instead in that constructor, as this is what we'll be given by both the windows and Mac OSX screensaver frameworks - Moved the handle_events logic out of the screensaver proper and into the demo program instead, as windows' screensaver framework (and mac osx's as well) will handle this for us

--- windows_nt.cpp
+++ windows_nt.cpp
@@ -7,75 +7,75 @@
 #include <SFML/Window.hpp>
 
 #include <windows.h>
+#include <scrnsave.h>
 
 #include "hexago/HexagoScreenSaver.hpp"
 
 
-static int run_preview(HWND window_handle, sf::ContextSettings settings) {
-    // hand over the window handle and settings to SFML
-    sf::RenderWindow window(window_handle, settings);
-    // instantiate the screensaver app with this window instance
-    hexago::HexagoScreenSaver screensaver(window);
-    // loop while window remains open and window handle remains valid
-    while(window.isOpen() && IsWindow(window_handle)) {
-        // call the update method to update internal state and draw the frame
-        screensaver.update();
+// windows timer handle
+#define TIMER 101
+
+/*
+ * for storing a dynamically-allocated SFML window which we'll use for the
+ * screensaver
+ * --this can't be automatically allocated because the creation and destruction
+ * of it happens in the ScreenSaverProc event-handler
+ */
+static sf::RenderWindow* window = NULL;
+// for storing the screensaver, dynamically-allocated for same reasons as above
+static hexago::HexagoScreenSaver* screensaver = NULL;
+
+
+// this is the main event-handling function of the windows screensaver framework
+extern "C" LONG WINAPI ScreenSaverProc(
+    HWND hWnd,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam
+) {
+    switch (message) {
+        case WM_CREATE:
+            // initialise the screensaver
+            window = new sf::RenderWindow(hWnd, sf::ContextSettings());
+            screensaver = new hexago::HexagoScreenSaver(window);
+            // start a window timer -the number is milliseconds of delay
+            SetTimer(hWnd, TIMER, 1000 / 30, NULL);
+            break;
+        case WM_TIMER:
+            if (screensaver != NULL) {
+                // render one frame of the screensaver
+                screensaver->update();
+            }
+            break;
+        case WM_DESTROY:
+            // stop the screensaver
+            // kill timer
+            KillTimer(hWnd, TIMER);
+            // relinquish resources
+            delete screensaver;
+            delete window;
+            break;
+        default:
+            // all other unhandled messages are passed to DefScreenSaverProc
+            return DefScreenSaverProc(hWnd, message, wParam, lParam);
     }
+    // for all the messages that we handle, return 0
     return 0;
 }
 
-
-static int run_screensaver(sf::ContextSettings settings) {
-    // get supported fullscreen video modes
-    std::vector<sf::VideoMode> video_modes = sf::VideoMode::getFullscreenModes();
-    // get first (best) fullscreen videomode and init window with it
-    sf::RenderWindow window(
-        video_modes[0], "Hexago", sf::Style::Fullscreen, settings
-    );
-    // hide the mouse cursor
-    window.setMouseCursorVisible(false);
-    // instantiate the screensaver app with this window instance
-    hexago::HexagoScreenSaver screensaver(window);
-    // loop while window remains open
-    while(window.isOpen()) {
-        // call the update method to update internal state and draw the frame
-        screensaver.update();
-    }
-    return 0;
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+extern "C" BOOL WINAPI ScreenSaverConfigureDialog(
+    HWND hDlg,
+    UINT message, 
+    WPARAM wParam,
+    LPARAM lParam
+) {
+    // there currentl is no dialog so we return false
+    return false;
 }
 
-
-int main(int argc, char* argv[]) {
-    // SFML window settings for later use
-    sf::ContextSettings settings;
-    // set anti-aliasing
-    settings.antialiasingLevel = 8;
-    // check command-line arguments
-    if(argc < 2) {
-        /*
-         * there wasn't at least one argument, which means config mode was
-         * requested - we don't support this (yet)
-         */
-        return 0;
-    }
-    // carry on and interrogate arguments
-    std::string option = argv[1]; // get second argument
-    if(option == "/s") {
-        // wahoo! regular screensaver mode :)
-        return run_screensaver(settings);
-    } else if(option == "/p") {
-        // this is preview mode, so we need to check the third argument
-        if(argc < 3) { // check for not enough arguments
-            fprintf(stderr, "Window handle required for preview mode\n");
-            return 1;
-        }
-        // convert decimal string to window handle
-        HWND window_handle = (HWND)atoi(argv[2]);
-        // run preview mode with this window handle
-        return run_preview(window_handle, settings);
-    } else if(option == "/c") {
-        // again, this is config mode (which we don't yet support)
-        return 0;
-    }
-    return 0;
+extern "C" BOOL WINAPI RegisterDialogClasses(HANDLE dlg) {
+    return true;
 }
+#pragma GCC diagnostic pop
