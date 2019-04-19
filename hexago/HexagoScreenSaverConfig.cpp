7c784075162f44eba19e275756e9f01df269c4cb
Joshua Saxby <joshua.a.saxby@gmail.com>
2019-04-19 09:09:49 +0100

    Add config option for antialiasing level Closes #60

--- hexago/HexagoScreenSaverConfig.cpp
+++ hexago/HexagoScreenSaverConfig.cpp
@@ -18,6 +18,7 @@ namespace hexago {
         ParameterRange<colour_channel_t> f_colour_channel_range,
         ParameterRange<colour_channel_t> alpha_colour_channel_range,
         unsigned int framerate,
+        unsigned int antialiasing,
         double minimum_screen_cover,
         hexagon_spawn_mode_t spawn_mode,
         background_colour_t background_colour
@@ -33,6 +34,7 @@ namespace hexago {
                 alpha_colour_channel_range
             ),
             framerate,
+            antialiasing,
             minimum_screen_cover,
             spawn_mode,
             background_colour
@@ -43,12 +45,14 @@ namespace hexago {
     HexagoScreenSaverConfig::HexagoScreenSaverConfig(
         HexagonFactoryConfig sub_config,
         unsigned int framerate,
+        unsigned int antialiasing,
         double minimum_screen_cover,
         hexagon_spawn_mode_t spawn_mode,
         background_colour_t background_colour
     )
       : HexagonFactoryConfig(sub_config)
       , framerate(framerate)
+      , antialiasing(antialiasing)
       , minimum_screen_cover(minimum_screen_cover)
       , spawn_mode(spawn_mode)
       , background_colour(background_colour)
